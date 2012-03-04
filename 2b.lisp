35eaf3557d93e9b92a56846f6404ce1c653347cc
adamminter4 <adamminter92@gmail.com>
2012-03-04 05:52:51 -0500

    Added passing and failing deftests for 1-3. Also created grammars that fail and win.

--- 2b.lisp
+++ 2b.lisp
@@ -1,9 +1,27 @@
 (defparameter *grammar* nil)
+(defparameter *testgrammarFAIL*
+  '((!Test -> (!Awesome !Dolphin)
+    (!Awesome -> (adam minter is awesome) !PaintingStuff)
+    (!PaintingStuff -> )
+    (!Lard ->  (i liek turtles) (i heard u liek mudkips))
+    (!Batman -> (better have his back broken by Bane in Dark Knight Rises))
+    (!Dolphin -> (dolphins rule))
+    (!RickSantorum -> ))))
 
-(deftest !terminalp ()
+(defparameter *testgrammarWIN*
+  '((!Test -> (!Awesome !Dolphin !Batman)
+     (!Awesome -> (adam minter is awesome) !Bro)
+     (!Bro -> (andrew butcher told me that tim and will are bros))
+     (!Awesome -> sweet dude (!Awesome !Awesome))
+     (!Batman -> (seriously better have his back broken in Dark Knight Rises)))))
+
+
+
+;1.) DefTests for Terminalp function;
+(deftest !terminalp0 ()
   (test t (terminalp "!fTw")))
 
-(deftest !terminalp ()
+(deftest !terminalp1 ()
   (test nil (terminalp "cat")))
 
 (defun terminalp (nonterm)
@@ -11,14 +29,29 @@
     (and (symbolp i)
 	 (eql (char (symbol-name i) 0) !\!))))
 
-(deftest !undefined-nonterminal (&aux (gram '((Grammar -> Awesome Lard Dolphin)
-					      (Awesome -> (adam minter is awesome) PaintingStuff)
-					      (Dolphin -> (dolphins rule) (RickSantorum)))))
-  (test Lard undefined-nonterminal(gram)))
+
+
+;2.) Deftests for Undefined-nonterminal function;
+(deftest !undefined-nonterminal0 () 
+  (test '(PaintingStuff RickSantourm) undefined-nonterminal *testgrammarFAIL*))
+
+(deftest !undefined-nonterminal1 ()
+  (test nil undefined-nonterminal *testgrammarWIN*))
 
 (defun undefined-nonterminal (g)
   (setf *grammar* g))
-  
 
+
+
+;3.) Deftests for Unused-Rewrite Function;
+(deftest !unused-rewrite () 
+  (test '(Lard Batman) (unused-rewrite *testgrammarFAIL)))
+
+(deftest !unused-rewrite ()
+  (test nil (unused-rewrite *testgrammarWIN*)))
+  
+(defun unused-rewrite (g)
+  (setf *grammar* g)
+  
 
    
