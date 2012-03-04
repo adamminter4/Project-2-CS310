0845b68c4d15556bbce86c9036d60fe81efb0bbe
adamminter4 <adamminter92@gmail.com>
2012-03-04 07:22:38 -0500

    Finished 2b.lisp deftests #1 and #2.

--- 2b.lisp
+++ 2b.lisp
@@ -1,8 +1,8 @@
 (defparameter *grammar* nil)
 (defparameter *testgrammarFAIL*
-  '((!Test -> (!Awesome !Dolphin)
-    (!Awesome -> (adam minter is awesome) !PaintingStuff)
-    (!PaintingStuff -> )
+  '((!Test -> (!Awesome !Dolphin !Basketball)
+    (!Awesome -> ((adam minter is awesome) !PaintingStuff))
+    (!StupidTerm ->   )
     (!Lard ->  (i liek turtles) (i heard u liek mudkips))
     (!Batman -> (better have his back broken by Bane in Dark Knight Rises))
     (!Dolphin -> (dolphins rule))
@@ -15,7 +15,8 @@
      (!Awesome -> sweet dude (!Awesome !Awesome))
      (!Batman -> (seriously better have his back broken in Dark Knight Rises)))))
 
-
+(defun rewrites2b (phrase)
+  (rule-rhs (assoc phrase *grammar*)))
 
 ;1.) DefTests for Terminalp function;
 (deftest !terminalp0 ()
@@ -27,20 +28,36 @@
 (defun terminalp (nonterm)
   (let ((i (char nonterm 0)))
     (and (symbolp i)
-	 (eql (char (symbol-name i) 0) !\!))))
-
+	 (eql (char (symbol-name i) 0) #\!))))
 
 
 ;2.) Deftests for Undefined-nonterminal function;
-(deftest !undefined-nonterminal0 () 
-  (test '(PaintingStuff RickSantourm) undefined-nonterminal *testgrammarFAIL*))
+(deftest !undefined-nonterminal0 (&aux (root (!Test))) 
+  (test '(!PaintingStuff !Basketball) (!undefined-nonterm-list root *testgrammarFAIL*)))
 
-(deftest !undefined-nonterminal1 ()
-  (test nil undefined-nonterminal *testgrammarWIN*))
+(deftest !undefined-nonterminal1 (&aux (root (!Test)))
+  (test nil (!undefined-nonterm-list root *testgrammarWIN*)))
 
-(defun undefined-nonterminal (g)
-  (setf *grammar* g))
+(defun generatesUndefTerm (10 phrase &optional (g *grammar*))
+  (dotimes (i n)
+    (setf *grammar* (eval g))
+    (undefined-nonterminal (phrase))))
 
+(defun undefined-nonterminal (phrase)
+  (let ((lst nil))
+    (cond ((listp phrase)
+	   (mappend #'undefined-nonterminal phrase))
+	  (((terminalp (car phrase))
+	    (if (eql (rule-rhs phrase) nil)  
+		(list (car phrase) lst)
+		(undefined-nonterminal (rewrites2b phrase)))))
+	  )
+    lst))
+
+(defun !undefined-nonterm-list (root g &optional (n 5))
+  (setf *grammar* g)
+  (reset-seed)
+  (remove-duplicates (list (mapcar #'undefined-nonterminal '(!Test !Test !Test)))))
 
 
 ;3.) Deftests for Unused-Rewrite Function;
