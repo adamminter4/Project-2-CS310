52feb7344a039f2fba7c062a107ac6d5d5c4a37a
adamminter4 <adamminter92@gmail.com>
2012-03-06 00:41:35 -0500

    4 lines. I got my 2c.lisp solution to caching to work in 4 lines. Take that, Tim.

--- 2c.lisp
+++ 2c.lisp
@@ -2,13 +2,22 @@
 
 (defparameter *Story_Hash* (make-hash-table))
 
+(defun initializeStoryCache ()
+  (setf (gethash 'cache *Story_Hash*) nil))
+
+(deftest !generates ()
+  (reset-seed)
+  (test nil (generates 10 'Start '*grammar4*)))
+
+(deftest !storyCache ()
+  (reset-seed)
+  (initializeStoryCache)
+  (storyCache 'BOY *grammar2*)
+  (test nil (generates 10 'sentence '*grammar*)))
+  
 (defun storyCache (nonterm g)
   (setf *grammar* g)
-  (setf (gethash 'cache *Story_Hash*) (random-elt (cdr (cdr nonterm))))
-  (dolist (i *grammar*)
-    (cond ((eql (car i) nonterm) 
-	   (dolist (j (cdr (cdr i)))
-	     (setf j (gethash 'cache *Story_Hash*)))
-	   (remove-duplicates i)))))
-    
+  (setf (gethash 'cache *Story_Hash*) (random-elt (cdr (cdr (assoc nonterm *grammar*)))))
+  (rplacd (assoc nonterm *grammar*) (list '-> (gethash 'cache *Story_Hash*))))
+	 
   
